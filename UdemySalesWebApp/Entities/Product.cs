c60e8dce9ae6a08cf85eb752ad2852493b1c29ea
Antonio Sucre <ajs2104@gmail.com>
2020-03-21 09:54:55 -0300

    Entities and Data Access Layer with EF

---
+++ UdemySalesWebApp/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemySalesWebApp.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int? Codigo { get; set; }
+        public string Description { get; set; }
+        public double Quantity { get; set; }
+        public decimal Price { get; set; }
+        [ForeignKey("Category")]
+        public int CodigoCategory { get; set; }
+        public Category Category { get; set; }
+        public ICollection<SaleProducts> Sale { get; set; }
+
+    }
+}
---
+++ UdemySalesWebApp/Entities/Sale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemySalesWebApp.Entities
+{
+    public class Sale
+    {
+        [Key]
+        public int? Codigo { get; set; }
+        public DateTime Date { get; set; }
+        
+        [ForeignKey("Client")]
+        public int CodigoClient { get; set; }
+        public decimal Total { get; set; }
+
+        public Client Client { get; set; }
+        public ICollection<SaleProducts> Products { get; set; }
+
+    }
+}
