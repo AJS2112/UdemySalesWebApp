134a378c78be8a42d5d175ae76dae4acbff61743
Antonio Sucre <ajs2104@gmail.com>
2020-03-21 22:06:41 -0300

    Product Registry

---
+++ UdemySalesWebApp/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UdemySalesWebApp.DAL;
+using UdemySalesWebApp.Entities;
+using UdemySalesWebApp.Models;
+
+namespace UdemySalesWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        private IEnumerable<SelectListItem> CategoryList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            list.Add(new SelectListItem()
+            {
+                Value = string.Empty,
+                Text = string.Empty
+            });
+
+            foreach (var item in mContext.Category.ToList())
+            {
+                list.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Description.ToString()
+                });
+            }
+
+            return list;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> list = mContext.Product.Include(x => x.Category).ToList();
+            mContext.Dispose();
+
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Registry(int? id)
+        {
+            ProductViewModel viewModel = new ProductViewModel();
+            viewModel.CategoryList = CategoryList(); 
+            if (id != null)
+            {
+                var entity = mContext.Product.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entity.Codigo;
+                viewModel.Description = entity.Description;
+                viewModel.Quantity = entity.Quantity;
+                viewModel.Price = entity.Price;
+                viewModel.CodigoCategory = entity.CodigoCategory;
+
+            }
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Registry(ProductViewModel entity)
+        {
+            if (ModelState.IsValid)
+            {
+                Product objProduct = new Product()
+                {
+                    Codigo = entity.Codigo,
+                    Description = entity.Description,
+                    Quantity = entity.Quantity,
+                    Price = (decimal)entity.Price,
+                    CodigoCategory = (int)entity.CodigoCategory
+                };
+
+                if (entity.Codigo == null)
+                {
+                    mContext.Product.Add(objProduct);
+                } else
+                {
+                    mContext.Entry(objProduct).State = EntityState.Modified;
+                }
+                mContext.SaveChanges();
+            }
+            else
+            {
+                entity.CategoryList = CategoryList();
+                return View(entity);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var ent = new Product() { Codigo = id};
+            mContext.Attach(ent);
+            mContext.Remove(ent);
+            mContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UdemySalesWebApp/Models/ProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemySalesWebApp.Models
+{
+    public class ProductViewModel
+    {
+        public int? Codigo { get; set; }
+        [Required(ErrorMessage ="The Product Description is Required!")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "The Product Quantity in Stock is Required!")]
+        public double Quantity { get; set; }
+        [Required(ErrorMessage = "The Product Price is Required!")]
+        [Range(0.1,Double.PositiveInfinity)]
+        public decimal? Price { get; set; }
+        [Required(ErrorMessage = "The Product Category is Required!")]
+        public int? CodigoCategory { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
