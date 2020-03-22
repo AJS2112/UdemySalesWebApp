9c58caeb52eb62b87fc35c06453af68173f4d0f7
Antonio Sucre <ajs2104@gmail.com>
2020-03-22 15:34:16 -0300

    Sales Registry

---
+++ UdemySalesWebApp/Controllers/SaleController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using UdemySalesWebApp.DAL;
+using UdemySalesWebApp.Entities;
+using UdemySalesWebApp.Models;
+
+namespace UdemySalesWebApp.Controllers
+{
+    public class SaleController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public SaleController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        private IEnumerable<SelectListItem> ClientList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            list.Add(new SelectListItem()
+            {
+                Value = string.Empty,
+                Text = string.Empty
+            });
+
+            foreach (var item in mContext.Client.ToList())
+            {
+                list.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Name.ToString()
+                });
+            }
+
+            return list;
+        }
+
+        private IEnumerable<SelectListItem> ProductList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            list.Add(new SelectListItem()
+            {
+                Value = string.Empty,
+                Text = string.Empty
+            });
+
+            foreach (var item in mContext.Product.ToList())
+            {
+                list.Add(new SelectListItem()
+                {
+                    Value = item.Codigo.ToString(),
+                    Text = item.Description.ToString()
+                });
+            }
+
+            return list;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Sale> list = mContext.Sale.ToList();
+            mContext.Dispose();
+
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Registry(int? id)
+        {
+            SaleViewModel viewModel = new SaleViewModel();
+            viewModel.ClientList = ClientList();
+            viewModel.ProductList = ProductList();
+
+            if (id != null)
+            {
+                var entity = mContext.Sale.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entity.Codigo;
+                viewModel.Date = entity.Date;
+                viewModel.CodigoClient = entity.CodigoClient;
+                viewModel.Total = entity.Total;
+            }
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Registry(SaleViewModel entity)
+        {
+            if (ModelState.IsValid)
+            {
+                Sale objSale = new Sale()
+                {
+                    Codigo = entity.Codigo,
+                    Date = (DateTime)entity.Date,
+                    CodigoClient = (int)entity.CodigoClient,
+                    Total = (decimal)entity.Total,
+                    Products = JsonConvert.DeserializeObject<ICollection<SaleProducts>>(entity.JsonProducts)
+                };
+
+                if (entity.Codigo == null)
+                {
+                    mContext.Sale.Add(objSale);
+                }
+                else
+                {
+                    mContext.Entry(objSale).State = EntityState.Modified;
+                }
+                mContext.SaveChanges();
+            }
+            else
+            {
+                entity.ClientList = ClientList();
+                entity.ProductList = ProductList();
+
+                return View(entity);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var ent = new Sale() { Codigo = id };
+            mContext.Attach(ent);
+            mContext.Remove(ent);
+            mContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("ReadProductPrice/{CodigoProduct}")]
+        public decimal ReadProductPrice(int CodigoProduct)
+        {
+            return mContext.Product.Where(x => x.Codigo == CodigoProduct)
+                                    .Select(x => x.Price)
+                                    .FirstOrDefault();
+        }
+    }
+}
---
+++ UdemySalesWebApp/Models/SaleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemySalesWebApp.Models
+{
+    public class SaleViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage ="The Sale Date is Required!")]
+        public DateTime Date { get; set; }  
+        [Required(ErrorMessage = "The Sale Client is Required!")]
+        public int CodigoClient { get; set; }
+
+        public decimal Total { get; set; }
+        public IEnumerable<SelectListItem> ClientList { get; set; }
+        public IEnumerable<SelectListItem> ProductList { get; set; }
+
+
+        public string JsonProducts { get; set; }
+
+    }
+}
