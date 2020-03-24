eb984a24f0b444a97db1b585f9ba572be188cadb
Antonio Sucre <ajs2104@gmail.com>
2020-03-23 21:49:39 -0300

    Sales Report with chart.js

---
+++ UdemySalesWebApp/Controllers/ReportController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UdemySalesWebApp.DAL;
+using UdemySalesWebApp.Models;
+
+namespace UdemySalesWebApp.Controllers
+{
+    public class ReportController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public ReportController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+        public IActionResult Graph()
+        {
+
+            var data = from sp in mContext.SaleProducts
+                       group sp by sp.CodigoProduct into SaleReport
+                       select new { 
+                           c = SaleReport.Key,
+                           t = SaleReport.Sum(x => x.Quantity),
+                       };
+                var q = from sg in data
+                       join p in mContext.Product on sg.c equals p.Codigo
+                        select new ReportViewModel()
+                        {
+                            CodigoProduct = sg.c,
+                            Description = p.Description,
+                            TotalSale = sg.t
+                        };
+
+            var list = q.ToList();
+
+            string values = string.Empty;
+            string labels = string.Empty;
+            string colors = string.Empty;
+            
+            var random = new Random();
+            for (int i = 0; i < list.Count; i++)
+            {
+                values += list[i].TotalSale.ToString() + ",";
+                labels += "'"+list[i].Description.ToString() + "',";
+                colors += "'" + String.Format("#{0:X6}", random.Next(0x1000000)) + "',";
+            }
+            
+            ViewBag.Values = values;
+            ViewBag.Labels = labels;
+            ViewBag.Colors = colors;
+
+            return View(); 
+        }
+    }
+}
---
+++ UdemySalesWebApp/Models/ReportViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemySalesWebApp.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace UdemySalesWebApp.Models
+{
+    public class ReportViewModel
+    {
+        public int CodigoProduct { get; set; }
+        public string Description { get; set; }
+        public double TotalSale { get; set; }
+
+        /*public List<ReportViewModel> List;
+
+        protected ApplicationDbContext mContext;
+
+        public ReportViewModel(ApplicationDbContext context)
+        {
+            mContext = context;
+
+            var list = mContext.SaleProducts.GroupBy(x => x.CodigoProduct).ToList();
+        }*/
+    }
+}
